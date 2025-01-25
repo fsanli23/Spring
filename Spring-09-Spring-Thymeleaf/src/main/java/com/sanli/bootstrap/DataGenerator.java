de6c5ba4393a264c0d0c79d068c9d38e55a77689
fsanli23 <sanlifatih23@gmail.com>
2025-01-25 15:28:46 -0600

    Add Spring-09 MVC thymeleaf

---
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/sanli/bootstrap/DataGenerator.java
@@ -0,0 +1,26 @@
+package com.sanli.bootstrap;
+
+import com.github.javafaker.Faker;
+import com.sanli.model.Student;
+
+import java.util.List;
+
+public class DataGenerator {
+    static Faker faker = new Faker();
+
+
+    public static List<Student> createStudent( ) {
+
+        return List.of(
+                new Student(faker.name().firstName(), faker.name().lastName(), faker.number().numberBetween(15, 40), faker.address().state()),
+                new Student(faker.name().firstName(), faker.name().lastName(), faker.number().numberBetween(15, 40), faker.address().state()),
+                new Student(faker.name().firstName(), faker.name().lastName(), faker.number().numberBetween(15, 40), faker.address().state()),
+                new Student(faker.name().firstName(), faker.name().lastName(), faker.number().numberBetween(15, 40), faker.address().state()),
+                new Student(faker.name().firstName(), faker.name().lastName(), faker.number().numberBetween(15, 40), faker.address().state())
+
+        );
+
+
+    }
+
+}
---
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/sanli/model/Student.java
@@ -0,0 +1,15 @@
+package com.sanli.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private String state;
+
+
+}
