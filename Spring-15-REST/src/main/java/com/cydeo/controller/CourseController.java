f8b6d77ea54ee9c81e474eedb7da60c2d523453c
fsanli23 <sanlifatih23@gmail.com>
2025-12-06 12:32:16 -0600

    Spring Rest

---
+++ Spring-15-REST/src/main/java/com/cydeo/controller/CourseController.java
@@ -0,0 +1,58 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.CourseDTO;
+import com.cydeo.service.CourseService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+//@Controller  --> Restcontroller include this
+@RestController  // --- > controller + ResponseBody
+@RequestMapping("/courses")
+
+public class CourseController {
+
+
+    private final CourseService courseService;
+
+    public CourseController(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+    public List<CourseDTO> getAllCourses() {
+        return courseService.getCourses();
+    }
+
+    @GetMapping("/{id}")
+    public CourseDTO getCourseById(@PathVariable("id") Long courseId) {
+        return courseService.getCourseById(courseId);
+    }
+
+    @GetMapping("/category/{name}")
+    public List<CourseDTO> getCoursesByCategory(@PathVariable("name") String category) {
+        return courseService.getCoursesByCategory(category);
+    }
+
+    @PostMapping
+    public CourseDTO createCourse(@RequestBody CourseDTO courseDTO) {
+        return courseService.createCourse(courseDTO);
+    }
+
+    @PutMapping("/{id}")
+    public void updateCourse(@PathVariable("id") Long id, @RequestBody CourseDTO courseDTO) {
+        courseService.updateCourse(id, courseDTO);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteCourse(@PathVariable("id") Long courseId) {
+        courseService.deleteCourseById(courseId);
+    }
+
+    @DeleteMapping
+    public void deleteCourses() {
+        courseService.deleteCourses();
+
+    }
+
+}
---
+++ Spring-15-REST/src/main/java/com/cydeo/controller/CourseController_ResponseEntity.java
@@ -0,0 +1,49 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.CourseDTO;
+import com.cydeo.service.CourseService;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/courses/api/v2")
+public class CourseController_ResponseEntity {
+
+
+    private final CourseService courseService;
+
+    public CourseController_ResponseEntity(CourseService courseService) {
+        this.courseService = courseService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CourseDTO>> getAllCourses() {
+        return ResponseEntity.
+                status(HttpStatus.ACCEPTED)
+                .header("Version", "Cydeo.V2")
+                .header("Operation", "Get list")
+                .body(courseService.getCourses());
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<CourseDTO> getCourseById(@PathVariable("id") Long id) {
+
+        return ResponseEntity.status(HttpStatus.ACCEPTED).body(courseService.getCourseById(id));
+
+    }
+
+    @PostMapping
+    public ResponseEntity<CourseDTO> createCourse(@RequestBody CourseDTO courseDTO) {
+
+        return ResponseEntity.status(HttpStatus.CREATED).header("Version", "Cydeo.V2")
+                .body(courseService.createCourse(courseDTO));
+
+    }
+
+
+}
