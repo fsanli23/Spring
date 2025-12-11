aa1047eceaac49c4cafaf992a0ea3925b04146f3
fsanli23 <sanlifatih23@gmail.com>
2025-12-10 19:38:26 -0600

    open Api

--- Spring-17-REST-ConsumingAPIs/src/main/java/com/cydeo/client/EmployeeClient.java
+++ Spring-17-REST-ConsumingAPIs/src/main/java/com/cydeo/client/EmployeeClient.java
@@ -5,8 +5,6 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-import java.util.List;
-
 @FeignClient(url = "https://dummyapi.io", name = "EMPLOYEE-CLIENT")
 public interface EmployeeClient {
     //6298ebfecd0551211fce37a6
